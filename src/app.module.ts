f1fe469d28fefb4dd67e43961816a5dc0a421faa
anuj-jft <anuj.singh@jellyfishtechnologies.com>
2024-01-27 22:19:46 +0530

    db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql-8df05ff-rsf-india.a.aivencloud.com',
+      port: 14450,
+      username: 'avnadmin',
+      password: 'AVNS_CkjxJO3cQghii3MxMSR',
+      database: 'defaultdb',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
